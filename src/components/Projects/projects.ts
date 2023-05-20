c17d1b86f4b0b867a6c636d0d2a31d9475b521dc
CmD0_ <elouan112@gmail.com>
2023-05-20 16:24:50 -0400

    updated projects list

--- src/components/Projects/projects.ts
+++ src/components/Projects/projects.ts
@@ -8,6 +8,16 @@ const projects = [
         name: 'Mflix-Flask',
         description: 'A Flask application that uses MongoDB to serve a movies database website.',
         URL: 'https://mflix.cmd0app.com',
+    },
+    {
+        name: 'projet_web_frontend_2023',
+        description: 'React application for a web final for the session of Winter 2023.',
+        URL: 'https://stage.cmd0app.com',
+    },
+    {
+        name: 'projet_web_backend_2023',
+        description: 'Node.js application using express that powers projet_web_frontend_2023.',
+        URL: 'https://api-stages.cmd0app.com/api/',
     }
 ];
 
